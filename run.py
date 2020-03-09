64c5940e3a3b2358a125d706bb0c1e7adaa24b1e
Sean Lewis <slewis100@gmail.com>
2020-03-09 21:49:39 +0000

    added increased functionality

--- run.py
+++ run.py
@@ -1,32 +1,38 @@
 import os
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template, request, session, url_for
+from datetime import datetime
 
 app = Flask(__name__)
-
+app.secret_key = 'randomstring123'
 messages= []
 
-def add_messages(username, message):
+def add_message(username, message):
     #Add messages to the messages list.
-    messages.append('{} : {}'.format(username, message))
+    now = datetime.now().strftime('%H:%M')
 
-def get_all_messages():
-    #Get all messages a seperate them with a br.
-    return '<br>'.join(messages)
+    messages.append({"timestamp": now, "from": username, "message": message})
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     # Main page with instructions
-    return 'To send a message use /USERNAME/MESSAGE'
+    if request.method == "POST":
+        session['username'] = request.form['username']
 
-@app.route('/<username>')
+    if 'username' in session:
+        return redirect(url_for('user', username=sesion['username']))
+
+    return render_template('index.html')
+
+@app.route('/chat/<username>', methods=['GET', 'POST'])
 def user(username):
-    # Display chat messages
-    return '<h1>Welcome, {0}</h1>{1}'.format(username, get_all_messages())
+    # Display & add chat messages
 
-@app.route('/<username>/<message>')
-def send_message(username, message):
-    # Create a new message and redirect to chat page
-    add_messages(username, message)
-    return redirect("/" + username)
+    if request.method == 'POST':
+        username = session['username']
+        message = request.form['message']
+        add_message(username, message)
+        return redirect(url_for('user', username=session['username']))
+
+    return render_template('chat.html', username = username, chat_messages = messages)
 
 app.run(host = os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', '5000')), debug=True)
